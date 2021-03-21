93fd7844efc59367c4893b8e8bdeae2da0d92057
Andres Almiray <aalmiray@gmail.com>
2021-03-21 18:59:34 +0100

    Init repository

---
+++ jreleaser.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 11
+//REPOS jitpack
+//DEPS com.github.jreleaser.jreleaser:jreleaser:main-SNAPSHOT
+
+public class jreleaser {
+    public static void main(String... args) throws Exception {
+        org.jreleaser.cli.Main.main(args);
+    }
+}
