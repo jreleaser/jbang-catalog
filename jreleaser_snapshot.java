53500d46e00b902967c420fb402124ef7af05bc5
jreleaserbot <jreleaser@kordamp.org>
2023-04-13 10:05:34 +0000

    jreleaser early-access

--- jreleaser_snapshot.java
+++ jreleaser_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 8+
 //REPOS jitpack
 //DEPS com.github.jreleaser.jreleaser:jreleaser:main-SNAPSHOT
-// Generated with JReleaser 1.6.0-SNAPSHOT at 2023-04-12T14:08:36.757778246Z
+// Generated with JReleaser 1.6.0-SNAPSHOT at 2023-04-13T10:02:16.504969983Z
 
 public class jreleaser_snapshot {
     public static void main(String... args) throws Exception {
