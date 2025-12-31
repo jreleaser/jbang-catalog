4f9c622ce9250b2e82cea5391f4b8cc688785241
jreleaserbot <jreleaser@kordamp.org>
2025-12-31 15:18:57 +0000

    jreleaser v1.22.0

--- jreleaser.java
+++ jreleaser.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS org.jreleaser:jreleaser:1.21.0
-// Generated with JReleaser 1.21.0-SNAPSHOT at 2025-10-31T09:02:17.092272113Z
+//DEPS org.jreleaser:jreleaser:1.22.0
+// Generated with JReleaser 1.22.0-SNAPSHOT at 2025-12-31T15:00:51.881964587Z
 
 public class jreleaser {
     public static void main(String... args) throws Exception {
